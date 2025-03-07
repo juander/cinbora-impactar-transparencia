b220eed9ef66687b44a61b3c5d393ce442426811
Victor Conde <168321661+ocondeoficial@users.noreply.github.com>
2025-03-07 20:41:11 -0300

    Add files via upload

--- frontend/src/components/ui/header.tsx
+++ frontend/src/components/ui/header.tsx
@@ -13,6 +13,12 @@ import {
 } from "@/components/ui/menubar";
 import dynamic from "next/dynamic";
 import cinLogo from "../../assets/cin.svg"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 const UserSidebar = dynamic(() => import("./user-sidebar").then((mod) => mod.UserSidebar), { ssr: false });
 
@@ -40,11 +46,10 @@ export default function Header() {
       </div>
       <Menubar className="border-none shadow-none text-white justify-between max-lg:hidden">
         <MenubarMenu>
-        <MenubarTrigger className="px-20 pl-0 text-xl font-semibold text-white transition-all duration-300 hover:text-[#d4dbf0] hover:scale-110">
+          <MenubarTrigger className="px-20 pl-0 text-xl font-semibold text-white transition-all duration-300 hover:text-[#d4dbf0] hover:scale-110">
             Início
           </MenubarTrigger>
-          
-          <MenubarTrigger className=" px-20 pr-0 text-xl font-semibold text-white transition-all duration-300 hover:text-[#d4dbf0] hover:scale-110">
+          <MenubarTrigger className="px-20 pr-0 text-xl font-semibold text-white transition-all duration-300 hover:text-[#d4dbf0] hover:scale-110">
             Ongs
           </MenubarTrigger>
         </MenubarMenu>
@@ -57,9 +62,20 @@ export default function Header() {
               Entrar
             </Button>
           </Link>
-          <div className="text-white text-4xl hidden max-lg:block">
-            ☰
-          </div>
+          <DropdownMenu>
+            <DropdownMenuTrigger className="text-white text-4xl hidden max-lg:block">
+              ☰
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="bg-[#00B3FF] rounded-xl border-solid border-2 border-white">
+              <DropdownMenuItem className="block text-center text-8 font-semibold text-white hover:scale-110">Início</DropdownMenuItem>
+              <DropdownMenuItem className="block text-center text-8 font-semibold text-white hover:scale-110">Ongs</DropdownMenuItem>
+              <DropdownMenuItem className="px-4 py-2 hover:bg-gray-200">
+                <Link href="/login">
+                  <Button className="bg-[#294BB6] text-8 font-semibold text-white rounded-xl px-14 py-3 transition-colors duration-300 delay-150 hover:bg-white hover:text-[#294BB6]">Entrar como Ong</Button>
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       )}
 
