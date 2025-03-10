e74b49c5d5207751001610b2e3ba1040d75b6c77
ocondeoficial <victorcondedev@gmail.com>
2025-03-10 08:01:42 -0300

    fix the routes

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,28 +1,18 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import Header from "../components/ui/header";
-import Main from "../components/ui/main";
 import Footer from "../components/ui/footer";
 import { ToastProvider } from "@/components/ui/toast-provider";
 import { CookieBanner } from "@/components/ui/cookie-banner";
 import "./globals.css";
 
-export default function RootLayout({
-  children,
-}: Readonly<{ children: React.ReactNode }>) {
-  const pathname = usePathname();
-  const isAuthPage = pathname === "/login";
-
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br" className="h-full">
       <body className="min-h-screen h-full flex flex-col">
         <ToastProvider />
         <Header />
-        <main className="flex-grow">
-          {!isAuthPage && <Main />}
-          {children}
-        </main>
+        <main className="flex-grow">{children}</main>
         <Footer />
         <CookieBanner />
       </body>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,247 @@
-export default function Home() {
+import fotoInicial from "../assets/starter.svg"
+import Image from "next/image"
+import boraImpactar from "../assets/bora_impactar.svg"
+import instagramIcon from "../assets/insta_icon.svg"
+import dateIcon from "../assets/date_icon.svg"
+import phoneIcon from "../assets/phone_icon.svg"
+import shareButton from "../assets/share.svg"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
+
+export default function Ongs() {
   return (
     <div>
+      <div className="flex mt-10">
+        <div className="flex flex-col w-3/5 text-[#294BB6] p-12">
+          <h1 className="font-semibold text-5xl ">Portal da Transparência</h1>
+          <h2 className="font-semibold text-3xl max-w-96 mt-auto">Transparência gera confiança, acompanhe cada passo das nossas atuações.</h2>
+        </div>
+        <div className="flex flex-col items-end">
+          <Image className="self-center mb-4" src={boraImpactar} alt="logo do projeto bora impactar" />
+          <Image className="rounded-l-full border-dashed border-r-0 border-4 border-[#007AFF]" src={fotoInicial} alt="foto de criança" />
+        </div>
+      </div>
+      <p className="mx-auto mt-24 text-center max-w-24 p-2 bg-[#009FE350] text-[#294BB6] mb-4">Ongs</p>
+      <p className="text-center mt-4 mb-4">Selecione sua ONG de interesse abaixo</p>
+
+      <div className="w-full max-w-md m-auto mb-12">
+        <Input
+          type="text"
+          placeholder="Search..."
+          className="w-full radius-4" />
+      </div>
+
+      <div className="max-w-7xl grid grid-cols-3 m-auto mb-16 gap-2 justify-items-center max-h-[560px] overflow-y-scroll">
+
+        <div className="max-w-sm border border-black flex flex-col p-2">
+          <h1 className="font-bold mb-2">Nome da Ong</h1>
+          <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
+          </p>
+          <div className="flex mb-2">
+            <Image src={dateIcon} alt="data" />
+            <p className="ml-2">2014</p>
+          </div>
+          <div className="flex mb-2">
+            <Image src={phoneIcon} alt="telefone" />
+            <p className="ml-2">(81)98429-5561</p>
+          </div>
+          <div className="flex mb-2">
+            <Image src={instagramIcon} alt="instagram" />
+            <p className="ml-2">ocondeoficial</p>
+          </div>
+
+          <div className="h-12 flex justify-between">
+            <Button className="w-4/5 h-full font-bold rounded-[34px] bg-[#294BB6] text-white border-solid border-[#2E4049] border">
+              TRANSPARÊNCIA
+            </Button>
+            <div className="w-2/12 rounded-full bg-[#F2F4F7] flex justify-center items-center">
+              <Image className="w-6 h-6" src={shareButton} alt="share" />
+            </div>
+
+          </div>
+        </div>
+
+        <div className="max-w-sm border border-black flex flex-col p-2">
+          <h1 className="font-bold mb-2">Nome da Ong</h1>
+          <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
+          </p>
+          <div className="flex mb-2">
+            <Image src={dateIcon} alt="data" />
+            <p className="ml-2">2014</p>
+          </div>
+          <div className="flex mb-2">
+            <Image src={phoneIcon} alt="telefone" />
+            <p className="ml-2">(81)98429-5561</p>
+          </div>
+          <div className="flex mb-2">
+            <Image src={instagramIcon} alt="instagram" />
+            <p className="ml-2">ocondeoficial</p>
+          </div>
+
+          <div className="h-12 flex justify-between">
+            <Button className="w-4/5 h-full font-bold rounded-[34px] bg-[#294BB6] text-white border-solid border-[#2E4049] border">
+              TRANSPARÊNCIA
+            </Button>
+            <div className="w-2/12 rounded-full bg-[#F2F4F7] flex justify-center items-center">
+              <Image className="w-6 h-6" src={shareButton} alt="share" />
+            </div>
+
+          </div>
+        </div>
+
+        <div className="max-w-sm border border-black flex flex-col p-2">
+          <h1 className="font-bold mb-2">Nome da Ong</h1>
+          <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
+          </p>
+          <div className="flex mb-2">
+            <Image src={dateIcon} alt="data" />
+            <p className="ml-2">2014</p>
+          </div>
+          <div className="flex mb-2">
+            <Image src={phoneIcon} alt="telefone" />
+            <p className="ml-2">(81)98429-5561</p>
+          </div>
+          <div className="flex mb-2">
+            <Image src={instagramIcon} alt="instagram" />
+            <p className="ml-2">ocondeoficial</p>
+          </div>
+
+          <div className="h-12 flex justify-between">
+            <Button className="w-4/5 h-full font-bold rounded-[34px] bg-[#294BB6] text-white border-solid border-[#2E4049] border">
+              TRANSPARÊNCIA
+            </Button>
+            <div className="w-2/12 rounded-full bg-[#F2F4F7] flex justify-center items-center">
+              <Image className="w-6 h-6" src={shareButton} alt="share" />
+            </div>
+
+          </div>
+        </div>
+
+        <div className="max-w-sm border border-black flex flex-col p-2">
+          <h1 className="font-bold mb-2">Nome da Ong</h1>
+          <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
+          </p>
+          <div className="flex mb-2">
+            <Image src={dateIcon} alt="data" />
+            <p className="ml-2">2014</p>
+          </div>
+          <div className="flex mb-2">
+            <Image src={phoneIcon} alt="telefone" />
+            <p className="ml-2">(81)98429-5561</p>
+          </div>
+          <div className="flex mb-2">
+            <Image src={instagramIcon} alt="instagram" />
+            <p className="ml-2">ocondeoficial</p>
+          </div>
+
+          <div className="h-12 flex justify-between">
+            <Button className="w-4/5 h-full font-bold rounded-[34px] bg-[#294BB6] text-white border-solid border-[#2E4049] border">
+              TRANSPARÊNCIA
+            </Button>
+            <div className="w-2/12 rounded-full bg-[#F2F4F7] flex justify-center items-center">
+              <Image className="w-6 h-6" src={shareButton} alt="share" />
+            </div>
+
+          </div>
+        </div>
+
+        <div className="max-w-sm border border-black flex flex-col p-2">
+          <h1 className="font-bold mb-2">Nome da Ong</h1>
+          <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
+          </p>
+          <div className="flex mb-2">
+            <Image src={dateIcon} alt="data" />
+            <p className="ml-2">2014</p>
+          </div>
+          <div className="flex mb-2">
+            <Image src={phoneIcon} alt="telefone" />
+            <p className="ml-2">(81)98429-5561</p>
+          </div>
+          <div className="flex mb-2">
+            <Image src={instagramIcon} alt="instagram" />
+            <p className="ml-2">ocondeoficial</p>
+          </div>
+
+          <div className="h-12 flex justify-between">
+            <Button className="w-4/5 h-full font-bold rounded-[34px] bg-[#294BB6] text-white border-solid border-[#2E4049] border">
+              TRANSPARÊNCIA
+            </Button>
+            <div className="w-2/12 rounded-full bg-[#F2F4F7] flex justify-center items-center">
+              <Image className="w-6 h-6" src={shareButton} alt="share" />
+            </div>
+
+          </div>
+        </div>
+
+        <div className="max-w-sm border border-black flex flex-col p-2">
+          <h1 className="font-bold mb-2">Nome da Ong</h1>
+          <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
+          </p>
+          <div className="flex mb-2">
+            <Image src={dateIcon} alt="data" />
+            <p className="ml-2">2014</p>
+          </div>
+          <div className="flex mb-2">
+            <Image src={phoneIcon} alt="telefone" />
+            <p className="ml-2">(81)98429-5561</p>
+          </div>
+          <div className="flex mb-2">
+            <Image src={instagramIcon} alt="instagram" />
+            <p className="ml-2">ocondeoficial</p>
+          </div>
+
+          <div className="h-12 flex justify-">
+            <Button className="w-4/5 h-full font-bold rounded-[34px] bg-[#294BB6] text-white border-solid border-[#2E4049] border">
+              TRANSPARÊNCIA
+            </Button>
+            <div className="w-2/12 rounded-full bg-[#F2F4F7] flex justify-center items-center">
+              <Image className="w-6 h-6" src={shareButton} alt="share" />
+            </div>
+
+            
+
+          </div>
+        </div>
+
+        <div className="max-w-sm border border-black flex flex-col p-2">
+          <h1 className="font-bold mb-2">Nome da Ong</h1>
+          <p className="mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nunc vulputate libero et velit interdum, ac aliquet odio mattis.
+          </p>
+          <div className="flex mb-2">
+            <Image src={dateIcon} alt="data" />
+            <p className="ml-2">2014</p>
+          </div>
+          <div className="flex mb-2">
+            <Image src={phoneIcon} alt="telefone" />
+            <p className="ml-2">(81)98429-5561</p>
+          </div>
+          <div className="flex mb-2">
+            <Image src={instagramIcon} alt="instagram" />
+            <p className="ml-2">ocondeoficial</p>
+          </div>
+
+          <div className="h-12 flex justify-between">
+            <Button className="w-4/5 h-full font-bold rounded-[34px] bg-[#294BB6] text-white border-solid border-[#2E4049] border">
+              TRANSPARÊNCIA
+            </Button>
+            <div className="w-2/12 rounded-full bg-[#F2F4F7] flex justify-center items-center">
+              <Image className="w-6 h-6" src={shareButton} alt="share" />
+            </div>
+
+          </div>
+        </div>
+
+      </div>
+
+
     </div>
   )
 }
